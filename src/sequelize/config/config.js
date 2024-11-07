3a162bfb6d3fa28c374214268c11c60771127bf4
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-07 15:22:58 +0400

    chore: change  config.js

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -18,5 +18,18 @@ module.exports = {
   production: {
     url: process.env.INTERNAL_DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
+    pool: {
+      max: 5, // Maximum number of connections in the pool
+      min: 0, // Minimum number of connections in the pool
+      acquire: 30000, // Maximum time in ms to get a connection
+      idle: 10000, // Maximum time in ms that a connection can be idle before being released
+    },
   },
 };
