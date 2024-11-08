5663ed1104fc97be039b5a9dc7f45a876f198d64
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-08 14:20:32 +0400

    change migration path in migration service

--- src/services/migration.service.js
+++ src/services/migration.service.js
@@ -2,21 +2,24 @@ const { exec } = require("child_process");
 
 const runMigrations = async (req, res) => {
   try {
-    exec("npx sequelize-cli db:migrate", (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Migration error ${error.message}`);
-        return res.status(500).json({ error: "Migration failed" });
+    exec(
+      "npx sequelize-cli db:migrate --migrations-path src/sequelize/migrations",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Migration error ${error.message}`);
+          return res.status(500).json({ error: "Migration failed" });
+        }
+        if (stderr) {
+          console.error(`Migration stderr: ${stderr}`);
+          return res.status(500).json({ error: "Migration failed." });
+        }
+        console.log(`Migration stdout: ${stdout}`);
+        res.json({
+          message: "Migrations executed successfully.",
+          output: stdout,
+        });
       }
-      if (stderr) {
-        console.error(`Migration stderr: ${stderr}`);
-        return res.status(500).json({ error: "Migration failed." });
-      }
-      console.log(`Migration stdout: ${stdout}`);
-      res.json({
-        message: "Migrations executed successfully.",
-        output: stdout,
-      });
-    });
+    );
   } catch (erorr) {
     console.log(erorr);
     res.status(500).json({ error: "Migration failed." });
