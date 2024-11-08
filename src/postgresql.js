ffc171793fa2543a1f21c47b2aeacecbb299e0d7
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-08 20:37:38 +0400

    1

--- src/postgresql.js
+++ src/postgresql.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.INTERNAL_DATABASE_URL, {
       rejectUnauthorized: false,
     },
   },
+  pool: {
+    max: 10, // Allows a larger number of concurrent connections (tune this based on expected load)
+    min: 2, // Keeps a few connections in reserve, which can improve response time for new queries
+    acquire: 60000, // Increases the max time to wait for a connection to 60 seconds (good for high-load apps)
+    idle: 20000, // Releases connections after 20 seconds of inactivity to reduce resource usage
+  },
 });
 
 const connection = async () => {
