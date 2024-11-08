092e826a58c4412d9a1c9adb534c3b9d09d2eafa
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-08 20:23:46 +0400

    add pool in sequelize conmnection

--- src/postgresql.js
+++ src/postgresql.js
@@ -3,10 +3,16 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.INTERNAL_DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    // ssl: {
+    //   require: true,
+    //   rejectUnauthorized: false,
+    // },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000, // Adjust this as needed
   },
 });
 
