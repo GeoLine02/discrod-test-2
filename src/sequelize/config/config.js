126213051f8283def815ffe22e63b8fdd96998af
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-09 14:40:24 +0400

    add ssl settings in config.js

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -19,17 +19,16 @@ module.exports = {
     url: process.env.INTERNAL_DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
-      // ssl: {
-      //   require: true,
-      //   rejectUnauthorized: false,
-      // },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-
     pool: {
-      max: 10, // Allows a larger number of concurrent connections (tune this based on expected load)
-      min: 2, // Keeps a few connections in reserve, which can improve response time for new queries
-      acquire: 60000, // Increases the max time to wait for a connection to 60 seconds (good for high-load apps)
-      idle: 20000, // Releases connections after 20 seconds of inactivity to reduce resource usage
+      max: 10,
+      min: 2,
+      acquire: 60000,
+      idle: 20000,
     },
   },
 };
